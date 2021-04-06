afac0a66777d902be24d91e621e82d6ae157083d
AlexandraIoana <alexandra.pletea@outlook.com>
2021-04-06 23:06:20 +0200

    Implemented hero details section on click of li

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 
 // Properties:
@@ -15,13 +16,17 @@ import { HEROES } from '../mock-heroes';
 export class HeroesComponent implements OnInit {
 
   heroes = HEROES;
+  selectedHero?: Hero;
 
   constructor() { }
 
   // It's called shortly after creating a component
   // It's a good place to initialise stuff
   ngOnInit(): void {
+  }
 
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
   }
 
 }
